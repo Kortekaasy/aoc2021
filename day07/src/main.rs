63afbae508bb66087ff739fe9d729387d435403a
Yoep Kortekaas <y.a.m.kortekaas@utwente.nl>
2021-11-30 16:20:03 +0100

    Preparatory commit for AoC 2021

---
+++ day07/src/main.rs
@@ -0,0 +1,28 @@
+// ========================= Challenge Logic ============================
+
+pub fn part1(input: String) -> String {
+    format!("Unimplemented!")
+}
+
+pub fn part2(input: String) -> String {
+    format!("Unimplemented!")
+}
+
+// =========================== Main Function ============================
+#[allow(dead_code)]
+fn main() {
+    formatted_print("A", part1(read_file("input")));
+    formatted_print("B", part2(read_file("input")));
+}
+
+pub fn read_file(file_name: &str) -> String {
+    return std::fs::read_to_string(file_name).expect(format!("File {} not found", file_name).as_str());
+}
+
+fn formatted_print(part: &str, output: String) {
+    println!("==================== Part {} ======================", part);
+    for l in output.lines() {
+        println!("| {:^46} |", l);
+    }
+    println!("==================================================");
+}
