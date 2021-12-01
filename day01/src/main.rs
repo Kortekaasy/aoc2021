0f1b153a746e53d7ae45bcd3c279e711914b04f1
Yoep Kortekaas <y.a.m.kortekaas@utwente.nl>
2021-12-01 09:26:06 +0100

    Solved day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,11 +1,65 @@
 // ========================= Challenge Logic ============================
 
 pub fn part1(input: String) -> String {
-    format!("Unimplemented!")
+    // Split input on newline and parse all lines as i64
+    let depths: Vec<i64> = input
+        .lines()
+        .map(|l|
+            l.parse::<i64>()
+             .expect("Could not parse input as i64")
+        )
+        .collect();
+    
+    // Compare every item i+1 with item i, and count how many item i+1's are larger than item i
+    let count = depths
+        .iter()
+        .skip(1)
+        .zip(
+            depths.iter()
+        )
+        .filter(|(&next, &current)| 
+            next > current
+        ).count();
+
+    format!("{}", count)
 }
 
 pub fn part2(input: String) -> String {
-    format!("Unimplemented!")
+    // Split input on newline and parse all lines as i64
+    let depths: Vec<i64> = input
+        .lines()
+        .map(|l|
+            l.parse::<i64>()
+             .expect("Could not parse input as i64")
+        )
+        .collect();
+    
+    // Compute the sums of the three-measurement windows
+    let sums = depths
+        .iter()
+        .skip(2)
+        .zip(
+            depths
+                .iter()
+                .skip(1)
+        )
+        .zip(depths.iter())
+        .map(|((&nextnext, &next), &current)| 
+            nextnext + next + current
+        ).collect::<Vec<i64>>();
+    
+    // Compare every windows with the previous window, and count how many windows are larger than the previous
+    let count = sums
+        .iter()
+        .skip(1)
+        .zip(
+            sums.iter()
+        )
+        .filter(|(&next, &current)| 
+            next > current
+        ).count();
+
+    format!("{}", count)
 }
 
 // =========================== Main Function ============================
