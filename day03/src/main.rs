0d71b7432d27d8ef2e717deebd0474242d963c79
Yoep Kortekaas <y.a.m.kortekaas@utwente.nl>
2021-12-03 08:52:46 +0100

    Yucky day3 code

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,11 +1,46 @@
 // ========================= Challenge Logic ============================
 
 pub fn part1(input: String) -> String {
-    format!("Unimplemented!")
+    let codes = input.lines().map(|l| i64::from_str_radix(l, 2).expect("Could not parse code")).collect::<Vec<i64>>();
+    let code_len = input.lines().next().unwrap().len();
+
+    let (gamma, epsilon) = (0..code_len).map(|shift| {
+        if codes.iter().map(|&c| c & (1 << shift)).filter(|&b| b > 0).count() > (codes.len() / 2) {
+            (1 << shift, 0)
+        } else {
+            (0, 1 << shift)
+        }
+    }).fold((0, 0), |(acc_gamma, acc_epsilon), (elem_gamma, elem_epsilon)| (acc_gamma + elem_gamma, acc_epsilon + elem_epsilon));
+    format!("{}", gamma * epsilon)
 }
 
 pub fn part2(input: String) -> String {
-    format!("Unimplemented!")
+    let codes = input.lines().map(|l| u64::from_str_radix(l, 2).expect("Could not parse code")).collect::<Vec<u64>>();
+    let code_len = input.lines().next().unwrap().len();
+
+    let mut ox_candidates = codes.clone();
+    let mut co2_candidates = codes.clone();
+
+    (0..code_len).rev().for_each(|shift| {
+        if ox_candidates.len() > 1 {
+            if ox_candidates.iter().map(|&c| c & (1 << shift)).filter(|&b| b > 0).count() * 2 >= (ox_candidates.len()) {
+                ox_candidates = ox_candidates.drain(..).filter(|&c| (c & (1 << shift)) > 0).collect();
+            } else {
+                ox_candidates = ox_candidates.drain(..).filter(|&c| (c & (1 << shift)) == 0).collect();
+            }
+        }
+
+        if co2_candidates.len() > 1 {
+            if co2_candidates.iter().map(|&c| c & (1 << shift)).filter(|&b| b > 0).count() * 2 >= co2_candidates.len() {
+                co2_candidates = co2_candidates.drain(..).filter(|&c| (c & (1 << shift)) == 0).collect();
+            } else {
+                co2_candidates = co2_candidates.drain(..).filter(|&c| (c & (1 << shift)) > 0).collect();
+            }
+        }
+    });
+    let ox = ox_candidates[0];
+    let co2 = co2_candidates[0];
+    format!("{}", ox * co2)
 }
 
 // =========================== Main Function ============================
