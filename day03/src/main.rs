967293fdec97227497053a5dfc534c4324ed0700
Yoep Kortekaas <y.a.m.kortekaas@utwente.nl>
2021-12-03 09:12:31 +0100

    Slightly less yucky implementation

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,43 +1,73 @@
 // ========================= Challenge Logic ============================
 
+pub fn most_common_bit_at_idx(v: &Vec<u64>, i: usize) -> u64 {
+    if v.iter().filter(|&c| c & (1 << i) > 0).count() * 2 >= v.len() {
+        1
+    } else {
+        0
+    }
+}
+
 pub fn part1(input: String) -> String {
-    let codes = input.lines().map(|l| i64::from_str_radix(l, 2).expect("Could not parse code")).collect::<Vec<i64>>();
+    // Parse codes from input
+    let codes = input.lines().map(|l| u64::from_str_radix(l, 2).expect("Could not parse code")).collect::<Vec<u64>>();
+    // Get number of bits per input line
     let code_len = input.lines().next().unwrap().len();
 
-    let (gamma, epsilon) = (0..code_len).map(|shift| {
-        if codes.iter().map(|&c| c & (1 << shift)).filter(|&b| b > 0).count() > (codes.len() / 2) {
+    // Compute the gamma and epsilon values
+    let (gamma, epsilon) = (0..code_len).rev().map(|shift| {
+        // if the most common bit == 1, return (2^shift, 0)
+        if most_common_bit_at_idx(&codes, shift) == 1 {
             (1 << shift, 0)
-        } else {
+        } else { // else return (0, 2^shift)
             (0, 1 << shift)
         }
-    }).fold((0, 0), |(acc_gamma, acc_epsilon), (elem_gamma, elem_epsilon)| (acc_gamma + elem_gamma, acc_epsilon + elem_epsilon));
+    }).fold( // Take the 2-powers from the map stage, and add them up to get gamma and epsilon
+        (0, 0), 
+        |(acc_gamma, acc_epsilon), (elem_gamma, elem_epsilon)| (acc_gamma + elem_gamma, acc_epsilon + elem_epsilon)
+    );
+
+    // Return gamma and epsilon
     format!("{}", gamma * epsilon)
 }
 
 pub fn part2(input: String) -> String {
+    // Parse codes from input
     let codes = input.lines().map(|l| u64::from_str_radix(l, 2).expect("Could not parse code")).collect::<Vec<u64>>();
+    // Get number of bits per input line
     let code_len = input.lines().next().unwrap().len();
 
+    // Make clone of codes for ox and co2 since we need to remove values
     let mut ox_candidates = codes.clone();
     let mut co2_candidates = codes.clone();
 
     (0..code_len).rev().for_each(|shift| {
+        // If there are more than 1 ox candidates
         if ox_candidates.len() > 1 {
-            if ox_candidates.iter().map(|&c| c & (1 << shift)).filter(|&b| b > 0).count() * 2 >= (ox_candidates.len()) {
+            // Check for most common bit at position `shift`
+            if most_common_bit_at_idx(&ox_candidates, shift) == 1 {
+                // if most common bit == 1, filter out elements that don't have a 1 at position `shift`
                 ox_candidates = ox_candidates.drain(..).filter(|&c| (c & (1 << shift)) > 0).collect();
             } else {
+                // if most common bit == 0, filter out elements that don't have a 0 at position `shift`
                 ox_candidates = ox_candidates.drain(..).filter(|&c| (c & (1 << shift)) == 0).collect();
             }
         }
 
+        // If there are more than 1 co2 candidates
         if co2_candidates.len() > 1 {
-            if co2_candidates.iter().map(|&c| c & (1 << shift)).filter(|&b| b > 0).count() * 2 >= co2_candidates.len() {
+            // Check for most common bit at position `shift`
+            if most_common_bit_at_idx(&co2_candidates, shift) == 1 {
+                // if most common bit == 1, filter out elements that don't have a 0 at position `shift`
                 co2_candidates = co2_candidates.drain(..).filter(|&c| (c & (1 << shift)) == 0).collect();
             } else {
+                // if most common bit == 0, filter out elements that don't have a 1 at position `shift`
                 co2_candidates = co2_candidates.drain(..).filter(|&c| (c & (1 << shift)) > 0).collect();
             }
         }
     });
+
+    // Take the oxygen and co2 values, and return the multiplication of them
     let ox = ox_candidates[0];
     let co2 = co2_candidates[0];
     format!("{}", ox * co2)
