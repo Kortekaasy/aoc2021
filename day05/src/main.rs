52762332350540a5e66c10a84c189ce89d51032a
Yoep Kortekaas <y.a.m.kortekaas@utwente.nl>
2021-12-06 10:56:26 +0100

    Solved day 5 and 6

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,11 +1,70 @@
 // ========================= Challenge Logic ============================
 
 pub fn part1(input: String) -> String {
-    format!("Unimplemented!")
+    let coords = input.lines().map(|l| {
+        let splitted = l.splitn(3, " ").collect::<Vec<&str>>();
+        let (start, _arrow, end) = (splitted[0], splitted[1], splitted[2]);
+        let (start_x, start_y) = start.split_once(",").map(|(x, y)| (x.parse::<usize>().expect("could not parse start x"), y.parse::<usize>(). expect("could not parse start y"))).expect("Could not parse start coordinates");
+        let (end_x, end_y) = end.split_once(",").map(|(x, y)| (x.parse::<usize>().expect("could not parse end x"), y.parse::<usize>(). expect("could not parse end y"))).expect("Could not parse end coordinates");
+        ((start_x, start_y), (end_x, end_y))
+    }).collect::<Vec<((usize, usize), (usize, usize))>>();
+    
+    let mut field: [[u32; 1000]; 1000] = [[0; 1000]; 1000];
+
+    for ((x1, y1), (x2, y2)) in coords {
+        if x1 != x2 && y1 != y2 {
+            continue;
+        }
+
+        let dx = if x1 < x2 { 1 } else if x1 == x2 { 0 } else  { -1 };
+        let dy = if y1 < y2 { 1 } else if y1 == y2 { 0 } else  { -1 };
+
+        let (mut x, mut y) = (x1, y1);
+        loop {
+            field[y][x] += 1;
+            x = (x as i64 + dx) as usize;
+            y = (y as i64 + dy) as usize;
+
+            if x == x2 && y == y2 {
+                field[y][x] += 1;
+                break;
+            }
+        }
+    }
+    
+    format!("{}", field.iter().map(|r| r.iter().filter(|&&c| c > 1).count()).sum::<usize>())
 }
 
 pub fn part2(input: String) -> String {
-    format!("Unimplemented!")
+    let coords = input.lines().map(|l| {
+        let splitted = l.splitn(3, " ").collect::<Vec<&str>>();
+        let (start, _arrow, end) = (splitted[0], splitted[1], splitted[2]);
+        let (start_x, start_y) = start.split_once(",").map(|(x, y)| (x.parse::<usize>().expect("could not parse start x"), y.parse::<usize>(). expect("could not parse start y"))).expect("Could not parse start coordinates");
+        let (end_x, end_y) = end.split_once(",").map(|(x, y)| (x.parse::<usize>().expect("could not parse end x"), y.parse::<usize>(). expect("could not parse end y"))).expect("Could not parse end coordinates");
+        ((start_x, start_y), (end_x, end_y))
+    }).collect::<Vec<((usize, usize), (usize, usize))>>();
+    
+    let mut field: [[u32; 1000]; 1000] = [[0; 1000]; 1000];
+
+    for ((x1, y1), (x2, y2)) in coords {
+
+        let dx = if x1 < x2 { 1 } else if x1 == x2 { 0 } else  { -1 };
+        let dy = if y1 < y2 { 1 } else if y1 == y2 { 0 } else  { -1 };
+
+        let (mut x, mut y) = (x1, y1);
+        loop {
+            field[y][x] += 1;
+            x = (x as i64 + dx) as usize;
+            y = (y as i64 + dy) as usize;
+
+            if x == x2 && y == y2 {
+                field[y][x] += 1;
+                break;
+            }
+        }
+    }
+    
+    format!("{}", field.iter().map(|r| r.iter().filter(|&&c| c > 1).count()).sum::<usize>())
 }
 
 // =========================== Main Function ============================
--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,11 +1,50 @@
 // ========================= Challenge Logic ============================
 
+pub struct LanternFish(i8);
+
 pub fn part1(input: String) -> String {
-    format!("Unimplemented!")
+    let mut fish: Vec<LanternFish> = input.split(",").map(|v| v.parse::<i8>().expect("Could not parse as i8")).map(|v| LanternFish(v)).collect();
+    for _ in 0..80 {
+        for i in 0..fish.len() {
+            if fish[i].0 == 0 {
+                fish.push(LanternFish(8));
+                fish[i].0 = 7;
+            } 
+            fish[i].0 -= 1;
+        }
+    }
+    format!("{}", fish.len())
 }
 
+use std::collections::HashMap;
+
 pub fn part2(input: String) -> String {
-    format!("Unimplemented!")
+    let end_day = 256;
+
+    let mut days: HashMap<i64, u128> = HashMap::new();
+    
+    input.split(",").for_each(|v| {
+        *days.entry(v.parse::<i64>().expect("Could not parse as i64") - 9).or_insert(0) += 1;
+    });
+
+    let mut min_key = *days.keys().min().expect("Expected minimum key to be available");
+
+    while (min_key + 9) < end_day {
+        let mut k = min_key;
+
+        *days.entry(k + 9).or_insert(0) += *days.entry(k).or_insert(0);
+        k += 9;
+
+        while k + 7 < end_day {
+            *days.entry(k + 7).or_insert(0) += days[&min_key];
+            k += 7;
+
+        }
+
+        min_key += 1;
+    }
+
+    format!("{}", days.values().sum::<u128>())
 }
 
 // =========================== Main Function ============================
