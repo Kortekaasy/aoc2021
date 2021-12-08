80311035a1890e74892cd5ef939cd8fd29eced78
Yoep Kortekaas <y.a.m.kortekaas@utwente.nl>
2021-12-08 13:46:18 +0100

    Finished day 7

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,18 +1,51 @@
 // ========================= Challenge Logic ============================
-
-pub fn part1(input: String) -> String {
-    format!("Unimplemented!")
+pub fn parse_input(input: String) -> Vec<i32> {
+    input
+        .split(",")
+        .map(|v| 
+            v.parse::<i32>()
+            .expect(format!("Could not parse input \"{}\" as i32", v).as_str())
+        ).collect()
 }
 
-pub fn part2(input: String) -> String {
-    format!("Unimplemented!")
+pub fn part1(input: &Vec<i32>) -> String {
+    let min = *input.iter().min().expect("Expected to find min");
+    let max = *input.iter().max().expect("Expected to find max");
+
+    let min_y = (min..max).map(|y|
+        input.iter().map(|x| (y-x).abs()).sum::<i32>()
+    ).min().expect("Expected to find minimum y values");
+
+    format!("{}", min_y)
+}
+
+pub fn cost(c: i32) -> i32 {
+    // assert!(c >= 0, "Cost func doesnt work for negative numbers");
+    (c * (c + 1)) / 2
+}
+
+pub fn part2(input: &Vec<i32>) -> String {
+    let min = *input.iter().min().expect("Expected to find min");
+    let max = *input.iter().max().expect("Expected to find max");
+
+    let min_y = (min..max).map(|y|
+        input.iter().map(|x| (y-x).abs()).map(cost).sum::<i32>()
+    ).min().expect("Expected to find minimum y values");
+
+    format!("{}", min_y)
 }
 
 // =========================== Main Function ============================
 #[allow(dead_code)]
 fn main() {
-    formatted_print("A", part1(read_file("input")));
-    formatted_print("B", part2(read_file("input")));
+    let sample = parse_input(read_file("sample"));
+    let input = parse_input(read_file("input"));
+
+    assert_eq!(part1(&sample), "37");
+    formatted_print("A", part1(&input));
+
+    assert_eq!(part2(&sample), "168");
+    formatted_print("B", part2(&input));
 }
 
 pub fn read_file(file_name: &str) -> String {
